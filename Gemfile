8b8e556b3ab8a46c2c6927042e089e38e7bfbec1
Stan Szeto <stanley.szeto@sbcglobal.net>
2014-11-22 10:30:43 -0800

    Update gem file

--- Gemfile
+++ Gemfile
@@ -38,6 +38,6 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
-# group :production do
-#   gem 'rails_12factor'
-# end
+group :production do
+  gem 'rails_12factor'
+end
--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 module Tinderforsound
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
-    # config.serve_static_assets = true
+    config.serve_static_assets = true
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
